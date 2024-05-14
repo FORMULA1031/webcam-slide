a7a2c6691c5990d09d95bd9752f251d17ab26834
Masamitsu Bando <bando@ktc.ac.jp>
2024-05-14 19:46:11 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BUILD ? "/kutc-publec-2024/" : "/",
+  base: process.env.VITE_BUILD ? "/webcam-slide/" : "/",
   build: {
     outDir: 'docs',
   },
